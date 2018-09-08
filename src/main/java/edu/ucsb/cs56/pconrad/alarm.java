ca4525d6f66273282dfa103184f701ad89111bc2
SeakyLuo <haitianluo@umail.ucsb.edu>
2018-09-07 22:08:56 -0700

    url needed to finalize

---
+++ src/main/java/edu/ucsb/cs56/pconrad/alarm.java
@@ -0,0 +1,39 @@
+class alarm{
+    private String time;
+    private String purpose;
+
+    public alarm(String time, String purpose){
+        this.time = time;
+        this.purpose = purpose;
+    }
+
+    @Override
+    public String toString(){
+        return "Time: " + time + "\nPurpose: " + purpose + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o == null)
+			return false;
+		if (!(o instanceof alarm))
+            return false;
+        alarm a = (alarm) o;
+        return time == o.time && purpose == o.purpose;
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+    public String toJson(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public static alarm toClass(String json){
+        Gson gson = new Gson();
+        return gson.fromJson(json, alarm.class);
+    }
+}
